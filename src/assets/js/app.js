f533370be5a45fd8d4f0767dd8d0129c19328941
xkored <xkored@gmail.com>
2020-09-06 00:57:18 +0400

    feat: end area page

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -43,6 +43,37 @@ $(() => {
 });
 
 $(() => {
+    $('.object__list').owlCarousel({
+        loop: false,
+        smartSpeed: 800,
+        lazyLoad: true,
+        margin: 30,
+        navText: ["<svg width='8' height='11' viewBox='0 0 8 11' xmlns='http://www.w3.org/2000/svg'><path d='M6.5 1L2 5.5L6.5 10' stroke-width='2'/></svg>", "<svg width='7' height='11' viewBox='0 0 7 11' xmlns='http://www.w3.org/2000/svg'><path d='M1 1L5.5 5.5L1 10' stroke-width='2'/></svg>"],
+        responsive : {
+            0 : {
+                dots: true,
+                items: 1,
+                autoHeight: true,
+            },
+            600 : {
+                dots: true,
+                items: 2,
+                autoHeight: true,
+            },
+            800 : {
+                dots: true,
+                items: 3,
+            },
+            1200 : {
+                nav: true,
+                dots: false,
+                items: 4,
+            },
+        }
+    });
+});
+
+$(() => {
     $('.object__slider').owlCarousel({
         nav: true,
         items: 1,
@@ -62,8 +93,12 @@ $(() => {
         dots: true,
         smartSpeed: 800,
         lazyLoad: true,
+        autoplay: true,
+        autoplayTimeout: 5000,
         animateIn: 'fadeIn',
         animateOut: 'fadeOut',
+        mouseDrag: false,
+        touchDrag: false,
         navText: ["<svg width='8' height='11' viewBox='0 0 8 11' xmlns='http://www.w3.org/2000/svg'><path d='M6.5 1L2 5.5L6.5 10' stroke-width='2'/></svg>", "<svg width='7' height='11' viewBox='0 0 7 11' xmlns='http://www.w3.org/2000/svg'><path d='M1 1L5.5 5.5L1 10' stroke-width='2'/></svg>"],
     });
     $('.js-banner__nav--prev').on('click', function bannerSliderPrev () {
@@ -71,5 +106,39 @@ $(() => {
     });
     $('.js-banner__nav--next').on('click', function bannerSliderNext () {
         $('.banner__slider').trigger('next.owl.carousel');
+    });
+});
+
+
+$(() => {
+    // Trigger CSS animations on scroll.
+    $(function($) {
+
+        // Function which adds the 'animated' class to any '.animatable' in view
+        const doAnimations = function() {
+
+            // Calc current offset and get all animatables
+            const offset = $(window).scrollTop() + $(window).height(),
+                $animatables = $('.animatable');
+
+            // Unbind scroll handler if we have no animatables
+            if ($animatables.length == 0) {
+                $(window).off('scroll', doAnimations);
+            }
+
+            // Check all animatables and animate them if necessary
+            $animatables.each(function(i) {
+                const $animatable = $(this);
+                if (($animatable.offset().top + $animatable.height() - 20) < offset) {
+                    $animatable.removeClass('animatable').addClass('animated');
+                }
+            });
+
+        };
+
+        // Hook doAnimations on scroll, and trigger a scroll
+        $(window).on('scroll', doAnimations);
+        $(window).trigger('scroll');
+
     });
 });
